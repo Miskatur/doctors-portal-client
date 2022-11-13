71509eb3d99cd453552a8de594c97451ad223d9b
Miskatur <Miskaturrahman34826@gmail.com>
2022-11-14 04:05:50 +0600

    ok

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,31 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import banner from '../../../assets/images/bg.png'
 import chair from '../../../assets/images/chair.png'
 import { DayPicker } from 'react-day-picker';
-import { format } from 'date-fns';
 
-const AppointmentBanner = () => {
-    const [selectedDate, setSelectedDate] = useState(new Date())
+const AppointmentBanner = ({ selectedDate, setSelectedDate }) => {
 
-    let footer = <p>Please pick a day.</p>;
-    if (selectedDate) {
-        footer = <p>You picked {format(selectedDate, 'PP')}.</p>;
-    }
 
     return (
         <div className='py-10 lg:py-40  bg-no-repeat bg-cover bg-center bg-transparent' style={{ backgroundImage: `url(${banner})` }}>
             <div className="hero">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <img src={chair} className=" rounded-lg lg:w-1/2" alt='' />
-                    <div className='lg:mr-10'>
+                    <div className='lg:mr-16'>
                         <DayPicker
                             mode='single'
                             selected={selectedDate}
-                            onSelect={setSelectedDate}
-                            footer={footer}
-
+                            onSelect={(data) => {
+                                if (data) {
+                                    setSelectedDate(data)
+                                }
+                            }}
                         />
-                        {/* <p>You have selected : {format(selectedDate, 'PP')}.</p> */}
                     </div>
 
                 </div>
